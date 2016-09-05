8bc7bd03f966f6ea43c030be74aa01382342a406
Agusti Fernandez <kud0gfx@gmail.com>
2016-09-04 21:56:27 -0500

    a

--- lib/tmdb/search/search.ex
+++ lib/tmdb/search/search.ex
@@ -2,42 +2,39 @@ defmodule Tmdb.Search do
   use Tmdb.Base
 
   def companies(query, params \\ %{}) do
-    params = Map.merge(params, %{"query" => query})
-    get!("search/company?#{URI.encode_query(params)}").body
+    search_api("company", query, params)
   end
 
   def collections(query, params \\ %{}) do
-    params = Map.merge(params, %{"query" => query})
-    get!("search/collection?#{URI.encode_query(params)}").body
+    search_api("collection", query, params)
   end
 
   def keywords(query, params \\ %{}) do
-    params = Map.merge(params, %{"query" => query})
-    get!("search/keyword?#{URI.encode_query(params)}").body
+    search_api("keyword", query, params)
   end
 
   def lists(query, params \\ %{}) do
-    params = Map.merge(params, %{"query" => query})
-    get!("search/list?#{URI.encode_query(params)}").body
+    search_api("list", query, params)
   end
 
   def movies(query, params \\ %{}) do
-    params = Map.merge(params, %{"query" => query})
-    get!("search/movie?#{URI.encode_query(params)}").body
+    search_api("movie", query, params)
   end
 
   def multi(query, params \\ %{}) do
-    params = Map.merge(params, %{"query" => query})
-    get!("search/multi?#{URI.encode_query(params)}").body
+    search_api("multi", query, params)
   end
 
   def persons(query, params \\ %{}) do
-    params = Map.merge(params, %{"query" => query})
-    get!("search/person?#{URI.encode_query(params)}").body
+    search_api("person", query, params)
   end
 
   def tv(query, params \\ %{}) do
-    params = Map.merge(params, %{"query" => query})
-    get!("search/tv?#{URI.encode_query(params)}&append_to_response=videos").body
+    search_api("tv", query, params)
+  end
+
+  defp search_api(endpoint, query, params \\ %{}) do
+  params = Map.merge(params, %{"query" => query})
+  get!("search/#{endpoint}?#{URI.encode_query(params)}").body
   end
 end
