d2ab828bb4473c93c11037d1a65064dce8de245e
Sean Abrahams <abrahams@gmail.com>
2018-07-11 09:02:06 -0700

    Allow poison 3.0

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Tmdb.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.8"},
-      {:poison, "~> 1.5 or ~> 2.0"}
+      {:poison, "~> 1.5 or ~> 2.0 or ~> 3.0"}
     ]
   end
 end
